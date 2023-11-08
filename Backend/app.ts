91c6d60bb1b8e8c2b32f3f7154264ed75c4f2ac4
Aditya Choudhury <adityasubham03@gmail.com>
2023-11-08 16:03:35 +0530

    Added auth routes and default routes

--- Backend/app.ts
+++ Backend/app.ts
@@ -2,12 +2,44 @@ import express, { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 import consola from "consola";
 import Config from "./Config";
+import Auth from "./Routes/Auth";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
+
+app.use(
+	cors({
+		credentials: true,
+		origin: ["https://frontend.unknownclub.me", "http://localhost:5173"],
+	})
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
 	res.send({
-		name: "Aditya Choudhury",
+		data: {
+			appName: "Get-Me-Through",
+			developedBy: "Aditya Choudhury",
+			maintainedBy: "Aditya Choudhury",
+			version: "1.0.0.0",
+		},
+		success: true,
 	});
-	next();
+});
+
+app.use("/api/auth", Auth);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+	res.send({
+		reason: "invalid-request",
+		message:
+			"The endpoint you wanna reach is not available! Please check the endpoint again",
+		success: false,
+	});
 });
 
 async function connectToDB() {
@@ -30,7 +62,7 @@ async function connectToDB() {
 }
 
 const startServer = async () => {
-	// await connectToDB();
+	await connectToDB();
 	const port = Config.PORT || 5000;
 	app.listen(port, () => {
 		consola.success({
