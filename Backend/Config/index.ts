d1c6cbdd3d34aa503519f5c18a476d2f5b9a9f9f
Aditya Choudhury <adityasubham03@gmail.com>
2023-11-08 16:01:27 +0530

    Added configurations

--- Backend/Config/index.ts
+++ Backend/Config/index.ts
@@ -2,11 +2,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  DB: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  DB: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB}?retryWrites=true&w=majority`,
   REQUEST_TIMEOUT: parseInt(process.env.REQUEST_TIMEOUT || '5000', 10), // Default to 5000 if not provided
   PORT: parseInt(process.env.PORT || '3000', 10),
-  JWT_SECRET: process.env.JWT_SECRET,
-  JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET || 'guujkyl98iyghjfytj',
+  JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET || 'guujkyl98iyghjfytjr32r',
   SMTP_HOST: process.env.SMTP_HOST,
   SMTP_USER: process.env.SMTP_USER,
   SMTP_PASS: process.env.SMTP_PASS,
