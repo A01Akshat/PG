a54e4f4ee8c0a78a28d3ffab4ef44b8a9242fe78
N03Nilay <rudranilay03@gmail.com>
2024-02-06 15:15:17 +0530

    rateings done

--- Client/src/More_Info_Page/RateReview.jsx
+++ Client/src/More_Info_Page/RateReview.jsx
@@ -8,6 +8,16 @@ import { toast } from 'react-toastify';
 import axios from 'axios';
 import Navbar from "../Navbar/Navbar";
 import "./More_info.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faAddressBook } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faBuildingColumns } from "@fortawesome/free-solid-svg-icons";
+import { faCouch } from "@fortawesome/free-solid-svg-icons";
+import { faWifi } from "@fortawesome/free-solid-svg-icons";
+import { faToiletPaper } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const accessToken = localStorage.getItem('token');
 
@@ -34,10 +44,10 @@ const More_Info_basic = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const url = `https://pgbackend.adityachoudhury.com/api/property/${s}`;
+                const url = `https://pgbackend.adityachoudhury.com/api/property/rating/658cee91815b6541149be9bf`;
                 const response = await axios.get(url);
                 if (response.status === 200) {
-                    console.log(response.data);
+                    console.log(response);
                     setData(response.data);
                     // toast("DONE");
                 }
@@ -56,9 +66,179 @@ const More_Info_basic = () => {
     const closeModal = () => {
         setIsModalOpen(false);
     };
+    const [rate1 , setrate1] = useState(false);
+    const [rate2 , setrate2] = useState(false);
+    const [rate3 , setrate3] = useState(false);
+    const [rate4 , setrate4] = useState(false);
+    const [rate5 , setrate5] = useState(false);
     return (
         <>
             <div className="rect-box">
+            <div className="wrap-review">
+                    <div className="property-rate">
+                        <p style={{fontSize:"1.2rem",fontFamily:"cursive"}}>Your Feedback matters a lot for Us !</p>
+                        <p style={{fontWeight:"600",marginTop:"2rem"}}>How you rate about this PG</p>
+                        <div className="rate-stars">
+                        {(rate1) ? (<FontAwesomeIcon icon={faStar} style={{color:"blue",height:"2rem" , width:"2rem"}} onClick={() => {
+                            setrate5(false)
+                            setrate4(false)
+                            setrate3(false)
+                            setrate2(false)
+                        }} />) : (<FontAwesomeIcon icon={faStar} style={{color:"red",height:"2rem" , width:"2rem"}} onClick={() => {
+                            setrate1(true)
+                        }} />)}
+
+{(rate2) ? (<FontAwesomeIcon icon={faStar} style={{color:"blue",height:"2rem" , width:"2rem"}} onClick={() => {
+                            setrate5(false)
+                            setrate4(false)
+                            setrate3(false)
+                            
+                        }} />) : (<FontAwesomeIcon icon={faStar} style={{color:"red",height:"2rem" , width:"2rem"}} onClick={() => {
+                            setrate2(true)
+                            setrate1(true)
+                        }} />)}
+
+{(rate3) ? (<FontAwesomeIcon icon={faStar} style={{color:"blue",height:"2rem" , width:"2rem"}} onClick={() => {
+                            setrate5(false)
+                            setrate4(false)
+                            
+                        }} />) : (<FontAwesomeIcon icon={faStar} style={{color:"red",height:"2rem" , width:"2rem"}} onClick={() => {
+                            setrate3(true)
+                            
+                            setrate2(true)
+                            setrate1(true)
+                        }} />)}
+
+{(rate4) ? (<FontAwesomeIcon icon={faStar} style={{color:"blue",height:"2rem" , width:"2rem"}} onClick={() => {
+                            setrate5(false)
+                           
+                        }} />) : (<FontAwesomeIcon icon={faStar} style={{color:"red",height:"2rem" , width:"2rem"}} onClick={() => {
+                            setrate4(true)
+                            setrate3(true)
+                            setrate2(true)
+                            setrate1(true)
+                        }} />)}
+
+{(rate5) ? (<FontAwesomeIcon icon={faStar} style={{color:"blue",height:"2rem" , width:"2rem"}} />) : (<FontAwesomeIcon icon={faStar} style={{color:"red",height:"2rem" , width:"2rem"}} onClick={() => {
+                            setrate5(true)
+                            setrate4(true)
+                            setrate3(true)
+                            setrate2(true)
+                            setrate1(true)
+                        }} />)}
+                        </div>
+                        <p style={{fontWeight:"600",marginTop:"0.5rem"}}>Please share your opinion about this PG</p>
+                        <textarea className='inp-reveiw' placeholder='Your Review' cols="30" rows="10"></textarea>
+
+                        <button className='btn-sendFeedback'>Send Feedback</button>
+
+                   
+                    </div>
+                    <div className="wrap-get-rate">
+                        <div className="username-rate">
+                            <div>
+                    <FontAwesomeIcon icon={faUser} className='user-icon-rate-get' />
+                    </div>
+                    <div>
+                    <div style={{display:"flex" , flexDirection:"column", gap:"0.1rem" , marginTop:"-0.2rem"}}>
+                        <p style={{fontWeight:"700"}}>Nilay Rudra</p>
+                        <div style={{marginTop:"-0.5rem"}}><FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        
+                        </div>
+                    </div>
+                    <p style={{width:"40rem"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis accusantium corrupti iure, reiciendis laboriosam veniam consectetur ducimus saepe quas, maiores minus </p>
+
+                    </div>
+                    </div> 
+
+                    <div className="username-rate">
+                            <div>
+                    <FontAwesomeIcon icon={faUser} className='user-icon-rate-get' />
+                    </div>
+                    <div>
+                    <div style={{display:"flex" , flexDirection:"column", gap:"0.1rem" , marginTop:"-0.2rem"}}>
+                        <p style={{fontWeight:"700"}}>Nilay Rudra</p>
+                        <div style={{marginTop:"-0.5rem"}}><FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        
+                        </div>
+                    </div>
+                    <p style={{width:"40rem"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis accusantium corrupti iure, reiciendis laboriosam veniam consectetur ducimus saepe quas, maiores minus </p>
+
+                    </div>
+                    </div>
+
+                    <div className="username-rate">
+                            <div>
+                    <FontAwesomeIcon icon={faUser} className='user-icon-rate-get' />
+                    </div>
+                    <div>
+                    <div style={{display:"flex" , flexDirection:"column", gap:"0.1rem" , marginTop:"-0.2rem"}}>
+                        <p style={{fontWeight:"700"}}>Nilay Rudra</p>
+                        <div style={{marginTop:"-0.5rem"}}><FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        
+                        </div>
+                    </div>
+                    <p style={{width:"40rem"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis accusantium corrupti iure, reiciendis laboriosam veniam consectetur ducimus saepe quas, maiores minus </p>
+
+                    </div>
+                    </div>
+
+                    <div className="username-rate">
+                            <div>
+                    <FontAwesomeIcon icon={faUser} className='user-icon-rate-get' />
+                    </div>
+                    <div>
+                    <div style={{display:"flex" , flexDirection:"column", gap:"0.1rem" , marginTop:"-0.2rem"}}>
+                        <p style={{fontWeight:"700"}}>Nilay Rudra</p>
+                        <div style={{marginTop:"-0.5rem"}}><FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        
+                        </div>
+                    </div>
+                    <p style={{width:"40rem"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis accusantium corrupti iure, reiciendis laboriosam veniam consectetur ducimus saepe quas, maiores minus </p>
+
+                    </div>
+                    </div>
+
+                    <div className="username-rate">
+                            <div>
+                    <FontAwesomeIcon icon={faUser} className='user-icon-rate-get' />
+                    </div>
+                    <div>
+                    <div style={{display:"flex" , flexDirection:"column", gap:"0.1rem" , marginTop:"-0.2rem"}}>
+                        <p style={{fontWeight:"700"}}>Nilay Rudra</p>
+                        <div style={{marginTop:"-0.5rem"}}><FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        <FontAwesomeIcon icon={faStar} style={{color:"blue",height:"1rem" , width:"1rem"}} />
+                        
+                        </div>
+                    </div>
+                    <p style={{width:"40rem"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis accusantium corrupti iure, reiciendis laboriosam veniam consectetur ducimus saepe quas, maiores minus </p>
+
+                    </div>
+                    </div>
+                    
+                    </div>
+
+                    
+                </div>
             </div>
 
         </>
